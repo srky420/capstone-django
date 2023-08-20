d497548510b175e6888d9396dc1e376cac402bd0
srky <srky420@gmail.com>
2023-08-20 12:21:44 +0500

    Add accounts page layout

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 
+app_name = "news"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("headlines/<str:category>", views.HeadlinesView.as_view(), name="headlines"),
